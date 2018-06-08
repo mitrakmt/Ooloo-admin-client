0a8a793a98969c7484bdf5f9d3e7852d1397abc2
Michael Mitrakos <mitrakmt@Michaels-MBP.fios-router.home>
2018-06-08 11:33:51 -0700

    feat: complete adding of questions

--- src/containers/questions/questions.js
+++ src/containers/questions/questions.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { getQuestions, addQuestion, deleteQuestion } from 'utils/questions';
+import { getInterests } from 'utils/interests';
+
+import FilteredMultiSelect from 'react-filtered-multiselect';
 
 import './questions.css';
 
@@ -26,17 +29,34 @@ class Questions extends Component {
         super(props, context);
 
         this.state = {
+            interests: {},
+            availableInterests: [],
             questions: [],
             question: '',
             topics: [],
-            difficulty: 1,
+            difficulty: '',
             answers: [],
             correctAnswer: '',
+            answer1: '',
+            answer2: '',
+            answer3: '',
+            answer4: '',
             image: null
         };
     }
 
     componentWillMount() {
+        getInterests()
+            .then(interests => {
+                let newInterests = {}
+                for (let i = 0; i < interests.length; i++) {
+                    newInterests[interests[0].id] = interests[0].name
+                }
+                this.setState({
+                    interests: newInterests,
+                    availableInterests: interests
+                })
+            })
         this.getQuestions()
     }
 
@@ -53,12 +73,21 @@ class Questions extends Component {
     addQuestion = () => {
         let question = this.state.question
         let topics = this.state.topics
+        let finalTopics = []
         let difficulty = this.state.difficulty
-        let answers = this.state.answers
+        let answers = [
+            this.state.answer1,
+            this.state.answer2,
+            this.state.answer3,
+            this.state.answer4
+        ]
         let correctAnswer = this.state.correctAnswer
         let image = this.state.image
+        for (let i = 0; i < topics.length; i++) {
+            finalTopics.push(topics[i].id)
+        }
         // TODO: catch missing info provided
-        addQuestion(question, topics, difficulty, answers, correctAnswer, image)
+        addQuestion(question, finalTopics, difficulty, answers, correctAnswer, image)
             .then(res => {
                 if (res.error) {
                     return;
@@ -92,6 +121,16 @@ class Questions extends Component {
         })
     }
 
+    handleDeselect = (index) => {
+        let topics = this.state.topics.slice()
+        topics.splice(index, 1)
+        this.setState({topics})
+    }
+     
+    handleSelectionChange = (topics) => {
+        this.setState({topics})
+    }
+
     updateState = (event) => {
         this.setState({
             [event.target.id]: event.target.value
@@ -108,6 +147,12 @@ class Questions extends Component {
     }
 
     render() {
+        let multiselectStyles = {
+            button: 'questions-addContainer-multiselect__button',
+            buttonActive: 'questions-addContainer-multiselect__button--active',
+            filter: 'questions-addContainer-multiselect__filter',
+            select: 'questions-addContainer-multiselect__select'
+          }
         return (
             <div className="questions">
                 <div className="questions-questionsContainer">
@@ -122,7 +167,39 @@ class Questions extends Component {
                     this.state.showAddContainerStatus &&
                         <div className="questions-addContainer">
                             <div className="column">
-                                <input className="questions-addContainer-input" type="text" placeholder="question" id="question" onChange={this.updateState} value={this.state.question} onKeyPress={this.handleKeyPress} />
+                                <textarea className="questions-addContainer-textarea" type="text" placeholder="question" id="question" onChange={this.updateState} value={this.state.question} />
+                                <div className="row">
+                                    <input className="questions-addContainer-input" type="text" placeholder="Difficulty (1-3)" id="difficulty" onChange={this.updateState} value={this.state.difficulty} />
+                                    <input className="questions-addContainer-input" type="text" placeholder="Correct Answer" id="correctAnswer" onChange={this.updateState} value={this.state.correctAnswer} />
+                                    <input className="questions-addContainer-input" type="text" placeholder="Answer 1" id="answer1" onChange={this.updateState} value={this.state.answer1} /> 
+                                </div>
+                                <div className="row">
+                                    <input className="questions-addContainer-input" type="text" placeholder="Answer 2" id="answer2" onChange={this.updateState} value={this.state.answer2} />
+                                    <input className="questions-addContainer-input" type="text" placeholder="Answer 3" id="answer3" onChange={this.updateState} value={this.state.answer3} />
+                                    <input className="questions-addContainer-input" type="text" placeholder="Answer 4" id="answer4" onChange={this.updateState} value={this.state.answer4} />
+                                </div>
+                                <div className="row">
+                                    <FilteredMultiSelect
+                                        onChange={this.handleSelectionChange}
+                                        options={this.state.availableInterests}
+                                        selectedOptions={this.state.topics}
+                                        textProp="name"
+                                        valueProp="id"
+                                        className="questions-addContainer-multiselect"
+                                        classNames={multiselectStyles}
+                                    />
+                                    <div className="questions-addContainer-selectedTopics">
+                                        {this.state.topics.length === 0 && <p>(nothing selected yet)</p>}
+                                        {this.state.topics.length > 0 && <ul>
+                                            {this.state.topics.map((topic, i) => <li key={topic.id}>
+                                            {`${topic.name} `}
+                                            <button type="button" className="questions-addContainer-selectedTopics-delete" onClick={() => this.handleDeselect(i)}>
+                                                &times;
+                                            </button>
+                                            </li>)}
+                                        </ul>}
+                                    </div>
+                                </div>
                             </div>
                             <button className="questions-addContainer-button" onClick={this.addQuestion}>Submit</button>
                         </div>
@@ -130,13 +207,24 @@ class Questions extends Component {
                 <div className="questions-listContainer">
                     {
                         this.state.questions.map((question, index) => (
-                            <div className="questions-listContainer-questionContainer" key={`questionsList-${question.id}`}>
+                            <div className="questions-listContainer-questionContainer" key={`questionsList-${ question.id }`}>
                                 <div>
-                                    <h3 className="questions-listContainer-questionContainer-name">{ question.question }</h3>
+                                    <h3 className="questions-listContainer-questionContainer-question">{question.question}</h3>
+                                    <h3 className="questions-listContainer-questionContainer-name">{
+                                        question.topics.map(topic => ( this.state.interests[topic] + ', ' ))
+                                    }</h3>
+                                    <div className="row questions-listContainer-questionContainer-answers">
+                                        {
+                                            question.answers.map(answer => (
+                                                <h3 key={`questionList-answer-${question.id}-${answer}`}>{ answer }</h3>
+                                            ))
+                                        }
+                                    </div>
                                     <div className="row">
-                                        <p className="questions-listContainer-questionContainer-info">{question.answer}</p>
+                                        <p className="questions-listContainer-questionContainer-correctAnswer">Correct Answer: { question.correctAnswer }</p>
                                     </div>
                                 </div>
+                                <h3 className="questions-listContainer-questionContainer-delete" onClick={() => {this.deleteQuestion(question.id, index)}}>x</h3>
                             </div>
                         ))
                     }
