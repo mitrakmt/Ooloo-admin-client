9fb90fde0bfaa123a4a095fdb9b38e9eb4c15efd
Michael Mitrakos <mitrakmt@Michaels-MBP.lan1>
2018-07-21 09:46:53 -0500

    update FE to show oxys tech

--- src/containers/home/components/loggedOutHome/loggedOutHome.js
+++ src/containers/home/components/loggedOutHome/loggedOutHome.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './loggedOutHome.css'
 
 const LoggedOutHome = () => (
   <div className="loggedOutHome">
-    <h1>Welcome! Please log in to continue.</h1>
+    <h1 className="loggedOutHome-heading">OOLOO APP</h1>
+    <h5 className="loggedOutHome-secondaryHeading">by Oxys Technologies</h5>
+    <h1 className="loggedOutHome-welcome">
+      Please{' '}
+      <Link to={'/login'} className="loggedOutHome-logIn">
+        log in
+      </Link>{' '}
+      to continue
+    </h1>
   </div>
 )
 
